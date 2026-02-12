9bc941f8520271f18cee254950ba5ea413bc22f3
Cadz <developer.dev@gridlock-games.com>
2026-02-12 11:51:10 +0800

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
 import gsap from "gsap";
-import { Draggable} from "gsap/Draggable";
+import { Draggable } from "gsap/Draggable";
 
-import {Navbar, Welcome, Dock} from "#components";
-import {Safari, Terminal, Resume, Finder, Text, Image, Contact} from "#windows";
+import { Navbar, Welcome, Dock, Ballpit, LiquidEther } from "#components";
+import { Safari, Terminal, Resume, Finder, Text, Image, Contact } from "#windows";
 
 gsap.registerPlugin(Draggable);
 
 const App = () => {
     return (
         <main>
+            <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: 0, overflow: 'hidden' }}>
+                <Ballpit
+                    count={50}
+                    gravity={0.01}
+                    friction={0.997}
+                    wallBounce={0.95}
+                    followCursor={false}
+                    colors={["#211C9A", "#344FEB", "#6E22FE"]}
+                />
+            </div>
+            {/* <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: 0, overflow: 'hidden' }}>
+                <LiquidEther
+                    colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+                    mouseForce={20}
+                    cursorSize={100}
+                    isViscous={false}
+                    viscous={30}
+                    iterationsViscous={32}
+                    iterationsPoisson={32}
+                    resolution={0.5}
+                    isBounce={false}
+                    autoDemo={true}
+                    autoSpeed={0.5}
+                    autoIntensity={2.2}
+                    takeoverDuration={0.25}
+                    autoResumeDelay={3000}
+                    autoRampDuration={0.6}
+                />
+            </div> */}
             <Navbar />
             <Welcome />
             <Dock />
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock.jsx";
 import WindowControls from "#components/WindowControls.jsx";
+import Ballpit from "#components/Ballpit.jsx";
+import LiquidEther from "#components/LiquidEther";
 
-export {Navbar, Welcome, Dock, WindowControls};
+export {Navbar, Welcome, Dock, WindowControls, Ballpit, LiquidEther};
